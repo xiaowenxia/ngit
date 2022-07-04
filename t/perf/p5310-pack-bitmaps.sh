cb6f651b16c112f1e49e5f3956576d5d4539ab0f
Abhradeep Chakraborty <chakrabortyabhradeep79@gmail.com>
2022-07-04 08:46:16 +0000

    p5310-pack-bitmaps.sh: remove pack.writeReverseIndex
    The previous change enables the `pack.writereverseindex` to see
    the effect of writing reverse index in the performance test.
    
    Remove the `pack.writeReverseIndex` configuration.
    
    Below is the result of performance test. Output format is in
    seconds.
    
    Test                                                  this tree
    ------------------------------------------------------------------------
    5310.4: repack to disk (lookup=false)               293.80(251.30+14.30)
    5310.5: simulated clone                             12.50(5.15+1.36)
    5310.6: simulated fetch                             1.83(2.90+0.23)
    5310.7: pack to file (bitmap)                       39.70(20.25+7.14)
    5310.8: rev-list (commits)                          1.00(0.60+0.13)
    5310.9: rev-list (objects)                          4.11(4.00+0.10)
    5310.10: rev-list with tag negated via --not        0.07(0.02+0.05)
             --all (objects)
    5310.11: rev-list with negative tag (objects)       0.23(0.16+0.06)
    5310.12: rev-list count with blob:none              0.27(0.18+0.08)
    5310.13: rev-list count with blob:limit=1k          6.41(5.98+0.41)
    5310.14: rev-list count with tree:0                 0.26(0.18+0.07)
    5310.15: simulated partial clone                    4.34(3.29+0.37)
    5310.19: repack to disk (lookup=true)               250.93(171.97+20.78)
    5310.20: simulated clone                            10.80(5.14+1.06)
    5310.21: simulated fetch                            0.71(0.79+0.16)
    5310.22: pack to file (bitmap)                      39.49(20.19+6.98)
    5310.23: rev-list (commits)                         0.81(0.48+0.09)
    5310.24: rev-list (objects)                         3.48(3.38+0.09)
    5310.25: rev-list with tag negated via --not        0.04(0.00+0.03)
             --all (objects)
    5310.26: rev-list with negative tag (objects)       0.22(0.16+0.05)
    5310.27: rev-list count with blob:none              0.22(0.16+0.05)
    5310.28: rev-list count with blob:limit=1k          6.21(5.76+0.29)
    5310.29: rev-list count with tree:0                 0.23(0.16+0.06)
    5310.30: simulated partial clone                    4.53(3.14+0.39)
    
    Tests 4-15 are without the use of lookup table. The rests are
    repeatation of the previous tests but using lookup table.
    
    Mentored-by: Taylor Blau <me@ttaylorr.com>
    Co-Mentored-by: Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
    Signed-off-by: Abhradeep Chakraborty <chakrabortyabhradeep79@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/perf/p5310-pack-bitmaps.sh
+++ t/perf/p5310-pack-bitmaps.sh
@@ -13,8 +13,7 @@ test_perf_large_repo
 # We intentionally use the deprecated pack.writebitmaps
 # config so that we can test against older versions of git.
 test_expect_success 'setup bitmap config' '
-	git config pack.writebitmaps true &&
-	git config pack.writeReverseIndex true
+	git config pack.writebitmaps true
 '
 
 test_bitmap () {
